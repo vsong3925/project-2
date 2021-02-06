a69f4758b602b947848ad543843867d51344da73
Vera Song <verasong@Veras-Air.fios-router.home>
2021-02-05 22:22:20 -0500

    add

--- draft/config.py
+++
@@ -1,5 +0,0 @@
-remote_db_endpoint = 'databootcamp.cwwtq0uxj1bi.us-east-2.rds.amazonaws.com'
-remote_db_port = '3306'
-remote_db_name = 'project2'
-remote_db_user = 'admin'
-remote_db_pwd = 'Germany311!'
--- static/app.js
+++ static/app.js
@@ -1,22 +1,106 @@
 d3.json("/home_values").then(data => {
     console.log(data)
 
+    // create variables for two counties
     var county1 = "Lee County"; 
+    var state1 = "FL"
+    
+    var county2 = "Arlington County";
+    var state2 = "VA" 
 
-    var result = data.filter(record => {
-        record.RegionName == county1
+    // filter data based on counties and states
+    var result1 = data.filter(record => (record.RegionName == county1) & (record.State == state1))
+    var result2 = data.filter(record => (record.RegionName == county2) & (record.State == state2))
 
-        console.log(result)
-    })
+    // console.log(result1)
+    
+    // select data for home values and years
+    
+    // result1.forEach(element => {
+    //     var years_county1 = Object.keys(element).slice(5, 305)
+    //     var values_county1 = Object.values(element).slice(5, 305);
+    // })
 
-    console.log(result)
+    // result2.forEach(element => {
+    //     var years_county2 = Object.keys(element).slice(5, 305)
+    //     var values_county2 = Object.values(element).slice(5, 305);
+    // })
+
+
+    function years1(result1) {
+        result1.forEach(element => {
+            var years_county1 = Object.keys(element).slice(5, 305)
+            return years_county1;
+        })
+    }
+
+    function values1(result1) {
+        result1.forEach(element => {
+            var values_county1 = Object.values(element).slice(5, 305);
+            return values_county1
+        })
+    }
+
+
+    function years2(result2) {
+        result2.forEach(element => {
+            var years_county2 = Object.keys(element).slice(5, 305)
+            return years_county2;
+        })
+    }
+
+    function values2(result2) {
+        result2.forEach(element => {
+            var values_county2 = Object.values(element).slice(5, 305);
+            return values_county2
+        })
+    }
+
+    
+    // Plotly
+    trace1 = {
+        type: 'scatter',
+        x: years1(result1),
+        y: values1(result1),
+        mode: 'lines',
+        name: 'Red',
+        line: {
+          color: 'rgb(219, 64, 82)',
+          width: 3
+        }
+      };
+      
+      trace2 = {
+        type: 'scatter',
+        x: years2(result2),
+        y: values2(result2),
+        mode: 'lines',
+        name: 'Blue',
+        line: {
+          color: 'rgb(55, 128, 191)',
+          width: 1
+        }
+      };
+      
+      var layout = {
+        width: 500,
+        height: 500
+      };
+      
+      var data = [trace1, trace2];
+      
+      Plotly.newPlot('myDiv', data, layout);
+    
+    
+   
+    
+    
+
 
 
 
     // // array of region names
-    // var regionName = data.map(r => r.RegionName);
-       
-    // var filteredData = data.filter(regionName == "Lee County")
+    // var regionName = data.map(r => r.RegionName)
 
 
 })
